2aa8872b957f60b9366d1a7eb8735af8889932ca
crystalyn <00crystalynthompson@gmail.com>
2026-05-12 20:48:46 -0600

    plant debugging

--- Assets/scripts/plants.cs
+++ Assets/scripts/plants.cs
@@ -1,7 +1,10 @@
+using System.Numerics;
 using UnityEngine;
+
 
 public class plants : MonoBehaviour
 {
+    public LayerMask groundLayer; 
     int count = 0;
 
     void Start()
@@ -14,16 +17,17 @@ public class plants : MonoBehaviour
     {
         count++;
         
-        if (count == 10 || count == 30 || count == 50 || count == 70 || count == 90)
+        // if count is a multiple of 10, check if on ground.
+        
+        if (count % 10 == 0)
         {
-            if (transform.rotation.z > 0.1f || transform.rotation.z < -0.1f)
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position, UnityEngine.Vector3.down, out hit, 4f, groundLayer))
             {
                 transform.localScale *= 0.9f; // Shrink plant by 10%
-                // if plant is too small, destroy it
                 if (transform.localScale.x < 0.2f)
                 {
-                    // destroy self
-                    Destroy(gameObject);
+                    Destroy(gameObject); // destroy small plant
                 } 
             }
             else if (transform.localScale.x < 1.0f)
@@ -32,16 +36,18 @@ public class plants : MonoBehaviour
             }
         }
         
-        if (count == 200 || count == 400 || count == 600 || count == 800 || count == 1000)
+        if (count % 100 == 0)
         {
-            // spawn baby plant
-            Vector3 offset = new Vector3(
-            Random.Range(-3f, 3f),
-            0
-            ,Random.Range(-3f, 3f)
-            );
-
-            Instantiate(gameObject, transform.position + offset, Quaternion.identity);
+            
+            if (transform.localScale.x >= 1.0f)
+            {
+                GameObject baby =Instantiate( // create new plant
+                    gameObject, 
+                    transform.position + new UnityEngine.Vector3( Random.Range(-3f, 3f),0f,Random.Range(-3f, 3f)), 
+                    UnityEngine.Quaternion.identity
+                );
+                baby.transform.localScale = UnityEngine.Vector3.one * 0.2f; // make baby small
+            }
         }
 
         if (count > 1000)
@@ -50,7 +56,7 @@ public class plants : MonoBehaviour
         }
         if (transform.localScale.x < 0.2f)
         {
-            Destroy(gameObject); // Destroy small plant
+            Destroy(gameObject); // Destroy old plant
         }
     }
 
