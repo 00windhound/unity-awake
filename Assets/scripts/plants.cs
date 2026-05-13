c2fb628d86ed4c6f3d9b118e24e3cc3ac38af139
crystalyn <00crystalynthompson@gmail.com>
2026-05-13 00:25:50 -0600

    trying to debug plant growth and placement

--- Assets/scripts/plants.cs
+++ Assets/scripts/plants.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class plants : MonoBehaviour
 {
     public LayerMask groundLayer; 
+    public Transform model;
     int count = 0;
 
     void Start()
     {
-
+        // change scale to baby size
+        resize(0.2f);
        
     }
 
@@ -18,63 +20,71 @@ public class plants : MonoBehaviour
     {
         count++;
         
-        // if count is a multiple of 10, check if on ground.
         
         if (count % 10 == 0)
         {
             bool seeground = false;
             bool isupright = true;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position + UnityEngine.Vector3.up, UnityEngine.Vector3.down, out hit, 10f, groundLayer))
+            RaycastHit hit;// if on the ground
+            if (Physics.Raycast(model.position + UnityEngine.Vector3.up, UnityEngine.Vector3.down, out hit, 10f, groundLayer))
             {
                 seeground = true;
             }
 
-            float upright = UnityEngine.Vector3.Dot(transform.up, UnityEngine.Vector3.up);
+            float upright = UnityEngine.Vector3.Dot(model.up, UnityEngine.Vector3.up);
             if (upright < 0.5f)
             {
                 isupright = false;
             }
 
-            UnityEngine.Debug.Log(seeground + " " + isupright);
+
             if (!seeground || !isupright)
             {
-                transform.localScale *= 0.9f; // Shrink plant by 10%
-                if (transform.localScale.x < 0.2f)
+                resize(0.99f); // Shrink plant by 1%
+                if (model.localScale.x < 0.2f)
                 {
                     Destroy(gameObject); // destroy small plant
                 } 
             }
             
-            else if (transform.localScale.x < 1.0f)
+            else if (model.localScale.x < 1.0f)
             {
-                transform.localScale *= 1.01f; // Grow plant by 1%
+                resize(1.01f); // Grow plant by 1%
             }
         }
         
         if (count % 100 == 0)
         {
             
-            if (transform.localScale.x >= 1.0f)
+            if (model.localScale.x >= 1.0f)
             {
                 GameObject baby =Instantiate( // create new plant
                     gameObject, 
                     transform.position + new UnityEngine.Vector3( Random.Range(-3f, 3f),0f,Random.Range(-3f, 3f)), 
                     UnityEngine.Quaternion.identity
                 );
-                baby.transform.localScale = UnityEngine.Vector3.one * 0.2f; // make baby small
             }
         }
 
         if (count > 1900)
         {
-            transform.localScale *= 0.99f; // Shrink plant by 1%
-        }
-        if (transform.localScale.x < 0.2f)
-        {
+            resize(0.99f);
+            if (model.localScale.x < 0.2f)
+            {
             Destroy(gameObject); // Destroy old plant
+            }
         }
     }
 
-
+    public void resize(float change)
+    {
+        // grow or shrink plant by change percent
+        UnityEngine.Vector3 scale = model.localScale;
+        scale *= change;
+        model.localScale = scale;
+        model.localPosition = new UnityEngine.Vector3(0f, scale.y / 2f, 0f);
+    }
 }
+
+
+// model.localPosition = new Vector3(0f, growth / 2f, 0f);
